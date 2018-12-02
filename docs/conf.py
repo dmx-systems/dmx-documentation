77d8a230ca992d930ead80aa492441f589764f80
Silke Meyer <silke@silkemeyer.net>
2018-12-02 12:52:22 +0100

    Testing CC License in Footer

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = u'DMX'
-copyright = u'2018 DMX Systems, https://dmx.berlin, Author: Silke Meyer <silke@dmx.berlin>, <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.'
+copyright = u'2018 DMX Systems, https://dmx.berlin, Author: Silke Meyer <silke@dmx.berlin>, License: Creative Commons Attribution-ShareAlike 4.0 International (https://creativecommons.org/licenses/by-sa/4.0/)'
 author = "Silke Meyer <silke@dmx.berlin>"
 
 # The short X.Y version
